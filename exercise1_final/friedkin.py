a3fdeba63c8ba9f621bc41517395f6ae8b0479f3
dangellotto <dany.ange@hotmail.com>
2021-07-18 13:03:13 +0200

    Update friedkin.py

--- exercise1_final/friedkin.py
+++ exercise1_final/friedkin.py
@@ -25,17 +25,19 @@ def FriedkinJohnsen(G):
                 if opinions[i] - prev_opinions[i] < 10**-5:
                     stop += 1
     
-        # str = "Time step %d: %d nodi stabili" % (t, stop)
-        # print(str)
+        str = "Time step %d: %d nodi stabili" % (t, stop)
+        print(str)
 
         if stop < G.number_of_nodes():
             stop = 0
         prev_opinions = opinions.copy()
         t += 1 # Update time step
 
-    print(t)
     return opinions
 
+# Friedkin-Johnsen dynamics implementation with fixed value of stubborness
+# for all the nodes of the net
+# DO NOT USE THIS FUNCTION - IT'S ONLY FOR TEST PURPOSES
 def FriedkinJohnsen(G, stubborness):
     belief = {i:random.random() for i in G.nodes()}
     # stubborness = {i:random.random() for i in G.nodes()}
