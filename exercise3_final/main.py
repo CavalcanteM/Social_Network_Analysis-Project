0025cda709b27aa99a4739995f9e7119c7177c16
CavalcanteM <58189792+CavalcanteM@users.noreply.github.com>
2021-07-19 16:29:56 +0200

    Exercise3Final e documentazione Exercise2
    Refactoring e pulizia dei file main.py, manipulator.py e selection.py. Documentazione dell'esercizio3final e dell'esercizio2 del middle term

--- exercise3_final/main.py
+++ exercise3_final/main.py
@@ -12,13 +12,12 @@ def random_vector(n):
     return v
 
 
-def load_graph(x):
+def load_graph():
 
     G = nx.Graph()
 
-    with open('../exercise2_final/nets/net_' + str(x), newline='') as csvfile:
+    with open('../exercise2_final/nets/net_13', newline='') as csvfile:
         rows = csv.reader(csvfile, delimiter=' ')
-        #next(rows)
 
         for row in rows:
             G.add_edge(row[0], row[1])
@@ -27,41 +26,12 @@ def load_graph(x):
 
 
 p = [0.05, 0.15, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]   # Vettore dei candidati
-#c = random.randint(0, len(p)-1)     # Candidato da favorire
-c = 2
+c = random.randint(0, len(p)-1)     # Candidato da favorire
 B = 200     # Numero di seed da selezionare
 b = random_vector(10000)     # Vettore delle single-peaked preference
-print(p[c])
+G = load_graph()
 
-for i in range(1, 18):
-    if i != 14:
-        G = load_graph(i)
-        start = time.time()
-        res = manipulation(G, p, c, B, b)
-        end = time.time() - start
-        print(end)
-        with open("result5.txt", "a") as f:
-            f.write("Rete" + str(i) + " con Shapley Closeness:\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[1]) + "\n")
-            f.write("------------------------------------------------------\n")
-            f.write("Rete" + str(i) + " con Shapley Degree:\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[2]) + "\n")
-            f.write("------------------------------------------------------\n")
-            f.write("Rete" + str(i) + " con Shapley Threshold(k=10):\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[3]) + "\n")
-            f.write("------------------------------------------------------\n")
-            f.write("Rete" + str(i) + " con Closeness:\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[4]) + "\n")
-            f.write("------------------------------------------------------\n")
-            f.write("Rete" + str(i) + " con CLOSENESS+DEGREE+THRESHOLD:\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[5]) + "\n")
-            f.write("------------------------------------------------------\n")
-            f.write("Rete" + str(i) + " con CLOSENESS+DEGREE+THRESHOLD+NORMAL_DEGREE:\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[6]) + "\n")
-            f.write("------------------------------------------------------\n")
-            f.write("Rete" + str(i) + " con CLOSENESS+DEGREE+THRESHOLD+NORMAL_CLOSENESS:\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[7]) + "\n")
-            f.write("------------------------------------------------------\n")
-            f.write("Rete" + str(i) + " con CLOSENESS+DEGREE+THRESHOLD+NORMAL_DEGREE_CLOSENESS:\n")
-            f.write("Risultati truthful: " + str(res[0]) + "   Risultati manipolati: " + str(res[8]) + "\n")
-            f.write("------------------------------------------------------\n\n")
+start = time.time()
+manipulation(G, p, c, B, b)
+end = time.time() - start
+print(end)
--- exercise3_final/selection.py
+++ exercise3_final/selection.py
@@ -1,13 +1,10 @@
-import networkx as nx
-from priorityq import PriorityQueue
-import math, random
+import math
 import networkx as nx
 from priorityq import PriorityQueue
 from joblib import Parallel, delayed
 import itertools as it
 from tqdm import tqdm
-import numpy as np
-import time
+from exercise1_final.shapley import shapley_degree, shapley_threshold
 
 
 # Utility used for split a vector data in chunks of the given size.
@@ -16,38 +13,6 @@ def chunks(data, size):
     idata = iter(data)
     for i in range(0, len(data), size):
         yield {k: data[k] for k in it.islice(idata, size)}
-
-
-def shapley_degree(G):
-    shapley_values = dict()
-    list_nodes = list(G.nodes())
-
-    for v in tqdm(range(len(list_nodes)), desc="Calcolo Shapley Degree in corso..."):
-        shapley_values[list_nodes[v]] = 1/(1+G.degree(list_nodes[v]))
-        for u in G.neighbors(list_nodes[v]):
-            shapley_values[list_nodes[v]] += 1/(1+G.degree(u))
-
-    pq = PriorityQueue()
-    for node in shapley_values.keys():
-        pq.add(node, -shapley_values[node])
-
-    return shapley_values, pq
-
-
-def shapley_threshold(G, k):
-    shapley_values = dict()
-    list_nodes = list(G.nodes())
-
-    for v in tqdm(range(len(list_nodes)), desc="Calcolo Shapley Threshold in corso..."):
-        shapley_values[list_nodes[v]] = min(1, k / (1 + G.degree(list_nodes[v])))
-        for u in G.neighbors(list_nodes[v]):
-            shapley_values[list_nodes[v]] += max(0, (G.degree(u) - k + 1) / ((G.degree(u) * (1 + G.degree(u)))))
-
-    pq = PriorityQueue()
-    for node in shapley_values.keys():
-        pq.add(node, -shapley_values[node])
-
-    return shapley_values, pq
 
 
 def bfs(G, u):
@@ -103,6 +68,7 @@ def shapley_closeness(G, nodes):
     return shapley_values
 
 
+# Parallel implementation of Shapley Closeness
 def shapley_closeness_parallel(G, j):
     with Parallel(n_jobs=j) as parallel:
         result = parallel(delayed(shapley_closeness)(G, X)
@@ -113,142 +79,38 @@ def shapley_closeness_parallel(G, j):
         for node in res:
             shapley_values[node] += res[node]
 
-    pq = PriorityQueue()
-    for node in shapley_values.keys():
-        pq.add(node, -shapley_values[node])
-
-    return shapley_values, pq
+    return shapley_values
 
 
-# The measure associated to each node is exactly its degree divided by number of nodes - 1
-def degree(G):
-    cen = dict()
-    for u in G.nodes():
-        cen[u] = G.degree(u) / (G.number_of_nodes() - 1)
-    return cen
-
-
-def sum_bfs(G, u):
-    visited = set()
-    visited.add(u)
-    queue = [u]
-    dist = dict()
-    dist[u] = 0
-    sum = 0
-
-    while len(queue) > 0:
-        v = queue.pop(0)
-        for w in G[v]:
-            if w not in visited:
-                visited.add(w)
-                queue.append(w)
-                dist[w] = dist[v]+1
-                sum += dist[w]
-
-    return sum
-
-
-# It computes the closeness on the subset nodes.
-def parallel_closeness(G, nodes):
-    closeness = dict()
-    for node in nodes:
-        closeness[node] = (G.number_of_nodes()-1)/sum_bfs(G, node)
-    return closeness
-
-
-# Parallel implementation of closeness centrality
-def parallel_closeness_centrality(G, j):
-    with Parallel(n_jobs=j) as parallel:
-        result = parallel(delayed(parallel_closeness)(G, X)
-                          for X in chunks(G.nodes(), math.ceil(G.number_of_nodes() / j)))
-
-    cl_value = dict()
-    pq = PriorityQueue()
-    for res in result:
-        for x in res.keys():
-            cl_value[x] = res[x]
-            pq.add(x, -res[x])
-
-    return cl_value, pq
-
-
-
+# Function that perform the selection of B nodes in according to the shapley closeness, shapley degree and shapley
+# threshold.
 def selector(G, B):
-    sp_c, pqs1 = shapley_closeness_parallel(G, 4)
-    sp_d, pqs2 = shapley_degree(G)
-    sp_t, pqs3 = shapley_threshold(G, 10)
-    deg = degree(G)
-    cl, pqs4 = parallel_closeness_centrality(G, 4)
+    # Compute the various shapley value
+    sh_cl = shapley_closeness_parallel(G, 4)
+    sh_d = shapley_degree(G)
+    sh_t = shapley_threshold(G, 5)
 
-    sp_3 = dict()
-    sp_deg = dict()
-    sp_cl = dict()
-    sp_deg_cl = dict()
+    # final dict that will contain the sum of the normalized shapley values
+    final_dict = dict()
 
-    max_c = max(sp_c.values())
-    min_c = min(sp_c.values())
-    for k in sp_c.keys():
-        sp_3[k] = (sp_c[k]-min_c)/(max_c-min_c)
-        sp_deg[k] = (sp_c[k]-min_c)/(max_c-min_c)
-        sp_cl[k] = (sp_c[k]-min_c)/(max_c-min_c)
-        sp_deg_cl[k] = (sp_c[k]-min_c)/(max_c-min_c)
+    max_c = max(sh_cl.values())
+    min_c = min(sh_cl.values())
+    max_d = max(sh_d.values())
+    min_d = min(sh_d.values())
+    max_t = max(sh_t.values())
+    min_t = min(sh_t.values())
+    for k in sh_cl.keys():
+        final_dict[k] = (sh_cl[k]-min_c)/(max_c-min_c)
+        final_dict[k] += (sh_d[k]-min_d)/(max_d-min_d)
+        final_dict[k] += (sh_t[k]-min_t)/(max_t-min_t)
 
-    max_d = max(sp_d.values())
-    min_d = min(sp_d.values())
-    for k in sp_d.keys():
-        sp_3[k] += (sp_d[k]-min_d)/(max_d-min_d)
-        sp_deg[k] += (sp_d[k]-min_d)/(max_d-min_d)
-        sp_cl[k] += (sp_d[k]-min_d)/(max_d-min_d)
-        sp_deg_cl[k] += (sp_d[k]-min_d)/(max_d-min_d)
+    pq = PriorityQueue()
+    for k in final_dict.keys():
+        pq.add(k, -final_dict[k])
 
-    max_t = max(sp_t.values())
-    min_t = min(sp_t.values())
-    for k in sp_t.keys():
-        sp_3[k] += (sp_t[k]-min_t)/(max_t-min_t)
-        sp_deg[k] += (sp_t[k]-min_t)/(max_t-min_t)
-        sp_cl[k] += (sp_t[k]-min_t)/(max_t-min_t)
-        sp_deg_cl[k] += (sp_t[k]-min_t)/(max_t-min_t)
+    # selection phase
+    seeds = []
+    while len(seeds) < B:
+        seeds.append(int(pq.pop()))
 
-    max_d = max(deg.values())
-    min_d = min(deg.values())
-    for k in deg.keys():
-        sp_deg[k] += (deg[k]-min_d)/(max_d-min_d)
-        sp_deg_cl[k] += (deg[k]-min_d)/(max_d-min_d)
-
-    max_cl = max(cl.values())
-    min_cl = min(cl.values())
-    for k in cl.keys():
-        sp_cl[k] += (cl[k] - min_cl) / (max_cl - min_cl)
-        sp_deg_cl[k] += (cl[k] - min_cl) / (max_cl - min_cl)
-
-    pqs5 = PriorityQueue()
-    pqs6 = PriorityQueue()
-    pqs7 = PriorityQueue()
-    pqs8 = PriorityQueue()
-    for k in sp_3.keys():
-        pqs5.add(k, -sp_3[k])
-        pqs6.add(k, -sp_deg[k])
-        pqs7.add(k, -sp_cl[k])
-        pqs8.add(k, -sp_deg_cl[k])
-
-    # selezioniamo i seed tra le varie Priority Queues
-    seeds1 = []     # B shapley closeness
-    seeds2 = []     # B shapley degree
-    seeds3 = []     # B shapley threshold
-    seeds4 = []
-    seeds5 = []
-    seeds6 = []
-    seeds7 = []
-    seeds8 = []
-    # Riempio seeds1 e seeds2 con B elementi
-    for _ in range(B):
-        seeds1.append(int(pqs1.pop()))
-        seeds2.append(int(pqs2.pop()))
-        seeds3.append(int(pqs3.pop()))
-        seeds4.append(int(pqs4.pop()))
-        seeds5.append(int(pqs5.pop()))
-        seeds6.append(int(pqs6.pop()))
-        seeds7.append(int(pqs7.pop()))
-        seeds8.append(int(pqs8.pop()))
-
-    return [seeds1, seeds2, seeds3, seeds4, seeds5, seeds6, seeds7, seeds8]
+    return seeds
