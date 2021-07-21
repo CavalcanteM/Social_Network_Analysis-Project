59da7c8bcae18af7a7fcc41e7a4831325f10cd1e
LMignella <63794379+LMignella@users.noreply.github.com>
2021-07-21 20:30:15 +0200

    Update hierarchical.py

--- exercise1/hierarchical.py
+++ exercise1/hierarchical.py
@@ -12,10 +12,10 @@ def hierarchical(G):
             pq.add( frozenset([frozenset([e[0]]), frozenset([e[1]])]) , 0)
 
     while len(clusters) >= 5:
-        s = list(pq.pop())
-        clusters.remove(s[0])
-        clusters.remove(s[1])
-        
+        s = list(pq.pop())      #Take the first element from the priority queue, the two elements obtained will be fused into a new cluster
+        clusters.remove(s[0])   #Remove from the clusters set the first element obtained
+        clusters.remove(s[1])   #Remove from the clusters set the second element obtained
+        #Update the distances between clusters in all occurrences where one of the 2 elements obtained appears, and replace the new cluster in its place
         for w in clusters:
             if frozenset([s[0],w]) in pq.entry_finder:
                 pq.remove(frozenset([s[0], w]))
@@ -30,7 +30,7 @@ def hierarchical(G):
                 pq.remove(frozenset([w, s[1]]))
                 pq.add(frozenset([s[0]|s[1], w]), max(len(s[0])+len(s[1]),len(w)))
 
-        clusters.add((s[0]|s[1]))
+        clusters.add((s[0]|s[1]))   #Add the new cluster into the set
     
     # We save each cluster in a different file
     save_clusters("HIERARCHICAL/optimized", clusters[0], clusters[1], clusters[2], clusters[3])
